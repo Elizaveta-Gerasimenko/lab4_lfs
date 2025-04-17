0f6438da7654466bc1b8181ccec7c68f6be10b56
Liza <m2401744@edu.misis.ru>
2025-04-17 17:02:16 +0300

    -

--- include/library.hpp
+++ include/library.hpp
@@ -2,34 +2,23 @@
 #include <deque>
 #include <vector>
 #include <string>
-
 #include <iostream>
 #include <vector>
 #include <memory>
 #include <algorithm>
-#include <gtest/gtest.h>
 
-// ------------------------------
-// Абстрактный интерфейс (должен быть реализован)
-// ------------------------------
 class FileSystemComponent {
 public:
     virtual ~FileSystemComponent() = default;
-    virtual int get_size() const = 0; // TODO: реализовать в наследниках
-    virtual void add_component(std::shared_ptr<FileSystemComponent> component) {} // Для папок
-    virtual void remove_component(std::shared_ptr<FileSystemComponent> component) {} // Для папок
+    virtual int get_size() const = 0;
+    virtual void add_component(std::shared_ptr<FileSystemComponent> component) {}
+    virtual void remove_component(std::shared_ptr<FileSystemComponent> component) {}
 };
-
-// ------------------------------
-// Ваш код ниже (реализуйте недостающие части)
-// ------------------------------
 
 class File : public FileSystemComponent {
 public:
     File(int size) : _size(size) {}
-
-    // TODO: реализовать getSize()
-    int get_size() const override { return 0; }
+    int get_size() const override { return _size; }
 
 private:
     int _size;
@@ -37,11 +26,21 @@ private:
 
 class Folder : public FileSystemComponent {
 public:
-    int get_size() const override { return 0; }
+    int get_size() const override {
+        int total_size = 0;
+        for (const auto& component : _components) {
+            total_size += component->get_size();
+        }
+        return total_size;
+    }
 
-    void add_component(std::shared_ptr<FileSystemComponent> component) override {}
+    void add_component(std::shared_ptr<FileSystemComponent> component) override {
+        _components.push_back(component);
+    }
 
-    void remove_component(std::shared_ptr<FileSystemComponent> component) override {}
+    void remove_component(std::shared_ptr<FileSystemComponent> component) override {
+        _components.erase(std::remove(_components.begin(), _components.end(), component), _components.end());
+    }
 
 private:
     std::vector<std::shared_ptr<FileSystemComponent>> _components;
